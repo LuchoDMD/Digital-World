ba1fe21351a4fd59f526627cde6eace6b91eceeb
XPLoXe <aleeeboki@gmail.com>
2021-05-21 17:06:34 -0300

    me olvidé de copy pastear el main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Enemy charizard = new Enemy(5, 500,200,50,25,100, 100, "charizard");
+        Partner pikachu = new Partner(5,300,500,100,10,50, "pikachu");
+
+        Batalla battle1 = new Batalla(pikachu, charizard);
+        battle1.comenzarBatalla();
 
     }
 }
