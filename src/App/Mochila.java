5182b752f959e00dae5ca219f1ac8f365a67b5de
XPLoXe <aleeeboki@gmail.com>
2021-06-10 13:06:25 -0300

    Clase entrenador e inventario funcional

--- src/App/Mochila.java
+++ src/App/Mochila.java
@@ -7,12 +7,45 @@ public class Mochila<T> {
 
     private List<T> bolsillo;
 
+    public List<T> getBolsillo() {
+        return bolsillo;
+    }
+
+    public void setBolsillo(List<T> bolsillo) {
+        this.bolsillo = bolsillo;
+    }
+
     public Mochila() {
         bolsillo = new ArrayList<>();
     }
 
+
     public void agregarItems(T item){
-        bolsillo.add(item);
+        boolean flag = false;
+        int aux = 0;
+        for(T variable : bolsillo){
+            if(variable == item){
+                flag = true;
+                break;
+            }
+            aux ++;
+        }
+        if (flag){
+            Item it = (Item) bolsillo.get(aux);
+            it.setStock(it.getStock() + 1);
+        }
+        else{
+            bolsillo.add(item);
+        }
+    }
+
+
+    public void usarItem(int index){
+
+    }
+
+    public T mostrarItem(int index){
+        return bolsillo.get(index);
     }
 
     @Override
