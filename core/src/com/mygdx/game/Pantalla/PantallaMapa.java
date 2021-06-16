2eb335636d923cbcbfa7e6a036708980e2ec61d8
ElDiegote <n17_94@hotmail.com>
2021-06-16 01:13:53 -0300

    murio pc

--- core/src/com/mygdx/game/Pantalla/PantallaMapa.java
+++ core/src/com/mygdx/game/Pantalla/PantallaMapa.java
@@ -19,13 +19,9 @@ public class PantallaMapa implements Screen {
     private SpriteBatch b;
     private EntradaMovimiento entada = new EntradaMovimiento(this);
     private Personaje crisEspalda,crisFrente,crisIzq,crisDer;
-    private Personaje heroEspalda,heroFrente,heroIzq,heroDer;
     private TextureRegion quieto;
-    private TextureRegion quietoH;
     private float xActual=37;
     private float yActual=97;
-    private float xActualH=xActual;
-    private float yActualH=yActual-60;
     private Texto frase;
     private int op=0;
     private Imagen mapa;
@@ -44,12 +40,6 @@ public class PantallaMapa implements Screen {
         crisDer  = new Personaje(Recursos.CRIS_DER,3,0.1f);
         quieto = new TextureRegion();
         frase=new Texto(Recursos.FUENTE1,13, Color.WHITE,false);
-
-        heroEspalda  = new Personaje(Recursos.HERO_M_ESPALDA,3,0.1f);
-        heroFrente  = new Personaje(Recursos.HERO_M_FRENTE,3,0.1f);
-        heroIzq  = new Personaje(Recursos.HERO_M_IZQ,3,0.1f);
-        heroDer  = new Personaje(Recursos.HERO_M_DER,3,0.1f);
-        quietoH = new TextureRegion();
     }
 
     @Override
@@ -59,9 +49,6 @@ public class PantallaMapa implements Screen {
         b.begin();
         mapa.dibujar();
         moverse();
-        //quietoH=heroFrente.personajeEspera();
-        //b.draw(quietoH,200,200);
-        //colision();
         System.out.println("x:" + xActual + "y:" + yActual);
         b.end();
     }
@@ -73,96 +60,59 @@ public class PantallaMapa implements Screen {
     }
 
     private void moverse(){
-        if (entada.isArriba()){
-            crisEspalda.setX(xActual);
-            crisEspalda.setY(yActual+2);
-            crisEspalda.render(b);
-            yActual= crisEspalda.getY();
 
-           /* heroEspalda.setX(xActualH);
-            heroEspalda.setY(yActualH+1);
-            heroEspalda.render(b);
-            yActualH=heroEspalda.getY();*/
-            op=1;
-        }
-
-        else if (entada.isAbajo()){
-            crisFrente.setX(xActual);
-            crisFrente.setY(yActual-2);
-            crisFrente.render(b);
-            yActual=crisFrente.getY();
-
-            /*heroFrente.setX(xActualH);
-            heroFrente.setY(yActualH-1);
-            heroFrente.render(b);
-            yActualH=heroFrente.getY();*/
-            op=2;
-        }
-        else if (entada.isIzquierda()){
-            crisIzq.setX(xActual-2);
-            crisIzq.setY(yActual);
-            crisIzq.render(b);
-            xActual=crisIzq.getX();
-
-           /*heroIzq.setX(xActualH-1);
-            heroIzq.setY(yActualH);
-            heroIzq.render(b);
-            xActualH=heroIzq.getX();*/
-            op=3;
-        }
-        else if (entada.isDerecha()){
-            crisDer.setX(xActual+2);
-            crisDer.setY(yActual);
-            crisDer.render(b);
-            xActual=crisDer.getX();
-
-            /*heroDer.setX(xActualH+1);
-            heroDer.setY(yActualH);
-            heroDer.render(b);
-            xActualH=heroDer.getX();*/
-
-            op=4;
-        }
-        else if(entada.isLetraE()){
-            frase.setPosition(xActual+65,yActual+57);
-            frase.setTexto("Donde va el padre,\nva el hijo.");
-            quieto=crisFrente.personajeEspera();
-            b.draw(quieto,xActual,yActual);
-            frase.dibujar();
-        }
-        else{
-            switch (op){
-                case 0:
-                case 1:
-                    quieto= crisEspalda.personajeEspera();
-                    b.draw(quieto,xActual,yActual);
-
-                    //quietoH=heroEspalda.personajeEspera();
-                    //b.draw(quietoH,xActualH,yActualH);
-                    break;
-                case 2:
-                    quieto=crisFrente.personajeEspera();
-                    b.draw(quieto,xActual,yActual);
-
-                    //quietoH=heroFrente.personajeEspera();
-                    //b.draw(quietoH,xActualH,yActualH);
-                    break;
-                case 3:
-                    quieto=crisIzq.personajeEspera();
-                    b.draw(quieto,xActual,yActual);
-
-                    //quietoH=heroIzq.personajeEspera();
-                    //b.draw(quietoH,xActualH,yActualH);
-                    break;
-                case 4:
-                    quieto=crisDer.personajeEspera();
-                    b.draw(quieto,xActual,yActual);
-
-                    //quietoH=heroDer.personajeEspera();
-                    //b.draw(quietoH,xActualH,yActualH);
-                    break;
+            if (entada.isArriba()) {
+                crisEspalda.setX(xActual);
+                crisEspalda.setY(yActual + 2);
+                crisEspalda.render(b);
+                yActual = crisEspalda.getY();
+                op = 1;
+            } else if (entada.isAbajo()) {
+                crisFrente.setX(xActual);
+                crisFrente.setY(yActual - 2);
+                crisFrente.render(b);
+                yActual = crisFrente.getY();
+                op = 2;
+            } else if (entada.isIzquierda()) {
+                crisIzq.setX(xActual - 2);
+                crisIzq.setY(yActual);
+                crisIzq.render(b);
+                xActual = crisIzq.getX();
+                op = 3;
+            } else if (entada.isDerecha()) {
+                crisDer.setX(xActual + 2);
+                crisDer.setY(yActual);
+                crisDer.render(b);
+                xActual = crisDer.getX();
+                op = 4;
             }
-        }
+            else if (entada.isLetraE()) {
+                frase.setPosition(xActual + 65, yActual + 57);
+                frase.setTexto("Donde va el padre,\nva el hijo.");
+                quieto = crisFrente.personajeEspera();
+                b.draw(quieto, xActual, yActual);
+                frase.dibujar();
+            } else {
+                switch (op) {
+                    case 0:
+                    case 1:
+                        quieto = crisEspalda.personajeEspera();
+                        b.draw(quieto, xActual, yActual);
+                        break;
+                    case 2:
+                        quieto = crisFrente.personajeEspera();
+                        b.draw(quieto, xActual, yActual);
+                        break;
+                    case 3:
+                        quieto = crisIzq.personajeEspera();
+                        b.draw(quieto, xActual, yActual);
+                        break;
+                    case 4:
+                        quieto = crisDer.personajeEspera();
+                        b.draw(quieto, xActual, yActual);
+                        break;
+                }
+            }
     }
 
     @Override
